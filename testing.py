e042c283db905e0901bfdd090b9ef15d7438f05e
Samuel Martin <samumartin@deloitte.com>
2023-02-10 17:12:30 -0600

    inital trial with VS code

---
+++ testing.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np 
+
+# Trying to play around with the data, get familiar with VS studio, 
+# and trying to figure out everything to do within the pandas module
+
+#Pull in the data frame
+df = pd.read_table('causes_of_death.tsv')
+df.head()
