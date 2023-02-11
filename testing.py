4f8bb97a31bb0d702d49c98bc8c9a34fbaadf482
Samuel Martin <samumartin@deloitte.com>
2023-02-10 21:44:46 -0600

    Added in males from Alabama

--- testing.py
+++ testing.py
@@ -4,6 +4,12 @@ import numpy as np
 # Trying to play around with the data, get familiar with VS studio, 
 # and trying to figure out everything to do within the pandas module
 
-#Pull in the data frame
+# Pull in the data frame
 df = pd.read_table('causes_of_death.tsv')
-df.head()
+
+# Get a list of all Males from Alabama
+males_alabama_df = df.loc(
+    (df['State'] == 'Alabama') 
+    & (df['Gender Code'] == 'M')
+    )
+
